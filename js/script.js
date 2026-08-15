0395792b5b96df8142fdb35b15732034d3463bb8
Afrin Borsha <cont.afrinborsha@gmail.com>
2026-08-15 19:57:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,117 @@
+//ex1
+function ShowDate() {
+	document.getElementById('sd').innerHTML = Date();
+}
+//ex2
+function replaceText() {
+	document.getElementById('rt').innerHTML = Date();
+}
+//ex3
+function replaceText2() {
+	document.getElementById('rt2').innerHTML ="Afrin Borsha"
+}
+//ex4
+function bOn() {
+	document.getElementById('bulb').src = "images/bulb2.gif"
+}
+function bOff() {
+	document.getElementById('bulb').src = "images/bulb1.gif"
+}
+//ex5
+function bShow() {
+	document.getElementById('bulb3').style.display = "block";
+}
+function bHide() {
+	document.getElementById('bulb3').style.display = "none";
+}	
+//ex6
+function myFunction() {
+	document.write("Result");
+}
+//ex7
+function info() {
+	let f_name ="Borsha";
+
+	let age = 23;
+
+    document.getElementById('h3').innerHTML ="Her name is "+f_name+".";
+}
+//ex8
+function result() {
+	let num1 = 5;
+
+	let num2 = 10;
+
+	let result = num1 + num2;
+
+    document.getElementById('h4').innerHTML =
+    "Output is"+" "+result;
+}
+//ex9
+function result1() {
+	let num;
+
+	num = 5;
+
+	num = 10;
+
+	num = num + num;
+
+    document.getElementById('h5').innerHTML =
+    "Output is"+" "+num;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
